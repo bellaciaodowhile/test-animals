bb2304c870ccf55298fcd600e34d189b450b42a8
bellaciaodowhile <125389712+bellaciaodowhile@users.noreply.github.com>
2023-04-25 01:12:11 +0200

    init

---
+++ assets/js/data.animals.js
@@ -0,0 +1,200 @@
+const dataAnimals = [
+    {
+        name: 'Ballena',
+        number: '00',
+    },
+    {
+        name: 'Delfín',
+        number: '0',
+    },
+    {
+        name: 'Carnero',
+        number: '1',
+    },
+    {
+        name: 'Toro',
+        number: '2',
+    },
+    {
+        name: 'Ciempies',
+        number: '3',
+    },
+    {
+        name: 'Alacrán',
+        number: '4',
+    },
+    {
+        name: 'León',
+        number: '5',
+    },
+    {
+        name: 'Rana',
+        number: '6',
+    },
+    {
+        name: 'Perico',
+        number: '7',
+    },
+    {
+        name: 'Ratón',
+        number: '8',
+    },
+    {
+        name: 'Águila',
+        number: '9',
+    },
+    {
+        name: 'Tigre',
+        number: '10',
+    },
+    {
+        name: 'Gato',
+        number: '11',
+    },
+    {
+        name: 'Caballo',
+        number: '12',
+    },
+    {
+        name: 'Mono',
+        number: '13',
+    },
+    {
+        name: 'Paloma',
+        number: '14',
+    },
+    {
+        name: 'Zorro',
+        number: '15',
+    },
+    {
+        name: 'Oso',
+        number: '16',
+    },
+    {
+        name: 'Pavo',
+        number: '17',
+    },
+    {
+        name: 'Burro',
+        number: '18',
+    },
+    {
+        name: 'Chivo',
+        number: '19',
+    },
+    {
+        name: 'Cochino',
+        number: '20',
+    },
+    {
+        name: 'Gallo',
+        number: '21',
+    },
+    {
+        name: 'Camello',
+        number: '22',
+    },
+    {
+        name: 'Cebra',
+        number: '23',
+    },
+    {
+        name: 'Iguana',
+        number: '24',
+    },
+    {
+        name: 'Gallina',
+        number: '25',
+    },
+    {
+        name: 'Vaca',
+        number: '26',
+    },
+    {
+        name: 'Perro',
+        number: '27',
+    },
+    {
+        name: 'Zamuro',
+        number: '28',
+    },
+    {
+        name: 'Elefante',
+        number: '29',
+    },
+    {
+        name: 'Caimán',
+        number: '30',
+    },
+    {
+        name: 'Lapa',
+        number: '31',
+    },
+    {
+        name: 'Ardilla',
+        number: '32',
+    },
+    {
+        name: 'Pescado',
+        number: '33',
+    },
+    {
+        name: 'Venado',
+        number: '34',
+    },
+    {
+        name: 'Jirafa',
+        number: '35',
+    },
+    {
+        name: 'Culebra',
+        number: '36',
+    }
+]
+const dataHours = [
+    {
+        hour: '9:00',
+        time: 'am'
+    },
+    {
+        hour: '10:00',
+        time: 'am'
+    },
+    {
+        hour: '11:00',
+        time: 'am'
+    },
+    {
+        hour: '12:00',
+        time: 'am'
+    },
+    {
+        hour: '01:00',
+        time: 'pm'
+    },
+    {
+        hour: '02:00',
+        time: 'pm'
+    },
+    {
+        hour: '03:00',
+        time: 'pm'
+    },
+    {
+        hour: '04:00',
+        time: 'pm'
+    },
+    {
+        hour: '05:00',
+        time: 'pm'
+    },
+    {
+        hour: '06:00',
+        time: 'pm'
+    },
+    {
+        hour: '07:00',
+        time: 'pm'
+    },
+]
---
+++ assets/js/form-animals.js
@@ -0,0 +1,214 @@
+console.log('Index Js')
+const closeModalAnimals = document.querySelector('.close-modal-animals')
+const numberAnimal = document.querySelector('.number-animal')
+const currentAnimal = document.querySelector('.current-animal')
+const valueAnimals = document.querySelector('input.value-animals')
+
+closeModalAnimals.addEventListener('click', (e) => {
+    e.preventDefault()
+    closeModalAnimals.parentElement.parentElement.style.display = 'none'
+    valueAnimals.value = ''
+});
+valueAnimals.addEventListener('keyup', (e) => {
+    if (e.target.value > 20) e.target.value = 20; // Valor máximo de apuesta por animal
+})
+
+// Form Animals Only
+const myBalance = 40; // Acá es mi balance
+const balance = document.querySelector('.form-total .balance span') // Acá es donde aárecerá mi balance
+balance.innerHTML = myBalance + ',00'
+let animalsSelected = []
+const animalsSelectedForm = document.querySelector('.animals-selected-form')
+const sendOnlyAnimals = document.querySelector('.main-modal-animals .btn-play-game.trigger-only-animals')
+let totalForm = document.querySelector('.total-balance span')
+sendOnlyAnimals.addEventListener('click', (e) => {
+    e.preventDefault()
+    const modal = sendOnlyAnimals.parentElement.parentElement
+    const input = sendOnlyAnimals.parentElement.querySelector('input')
+    const alert = sendOnlyAnimals.parentElement.parentElement.querySelector('.alert')
+    const path = sendOnlyAnimals.parentElement.parentElement.querySelector('.current-animal').getAttribute('src')
+    const name = sendOnlyAnimals.parentElement.parentElement.querySelector('.current-animal').getAttribute('alt')
+    if (input.value < 1 || input.value > 20) { // Valor mínimo y máximo de apuesta por animal
+        modal.style.boxShadow = '0 15px 40px red'
+        alert.style.display = 'block'
+        setTimeout(() => {
+            modal.style.boxShadow = '0px 10px 20px #131915'
+            alert.style.display = 'none'
+        }, 3000)
+    } else {
+        animalsSelected.push({
+            path,
+            name,
+            value: input.value
+        })
+        modal.style.boxShadow = '0px 15px 40px var(--green)'
+        setTimeout(() => {
+            modal.style.boxShadow = '0px 10px 20px #131915'
+            modal.parentElement.style.display = 'none'
+            input.value = ''
+        }, 500)
+        animalsSelectedFunc()
+        const total = animalsSelected.reduce((previous, current) => {
+            return parseInt(previous) + parseInt(current.value);
+        }, 0);
+        totalForm.innerHTML = total + ',00'
+    }
+})
+
+function deleteItem(e, name) {
+    if (confirm(`¿Seguro que quiere eliminar al ${ name } de sus apuestas?`))  {
+        e.target.parentElement.remove()
+        animalsSelected = animalsSelected.filter(x => {
+            if (x.name != name) { return x }
+        })
+        const total = animalsSelected.reduce((previous, current) => {
+            return parseInt(previous) + parseInt(current.value);
+        }, 0);
+        totalForm.innerHTML = total + ',00'
+    }
+}
+function animalsSelectedFunc() {
+    animalsSelectedForm.innerHTML = ''
+    animalsSelected.map(el => {
+        animalsSelectedForm.innerHTML += `
+        <div class="item-animals-selected">
+            <img src="${ el.path }">
+            <div>${ el.name }</div>
+            <span>${ el.value }</span>
+            <i class="material-icons" onclick="deleteItem(event, '${ el.name }')">delete</i>
+        </div>` 
+    })
+}
+// Data Animals
+const dataAnimalsContent = document.querySelector('.data-animals-content')
+dataAnimals.map((el, index) => {
+    dataAnimalsContent.innerHTML += `
+    <div class="item-animals">
+        <img src="./assets/img/animals/${el.number}.png" alt="${el.name}">
+    </div>`
+})
+// Data Hours
+const dataHoursContent = document.querySelector('.data-hours-content')
+dataHours.map((el, index) => {
+    dataHoursContent.innerHTML += `
+    <div class="item-hour active" time="${el.hour}">
+        <img src="./assets/img/animals/helpers/checked.png">
+        ${el.hour} ${el.time}
+    </div>`
+})
+let hoursSelected = []
+const itemsHours = [...document.querySelectorAll('.grid-animals .item-hour.active')]
+const hoursSelectedForm = document.querySelector('.hours-selected-form')
+const stopHours = [
+    '8:50',
+    '9:50',
+    '10:50',
+    '11:50',
+    '12:50',
+    '1:50',
+    '2:50',
+    '3:50',
+    '4:50',
+    '5:50',
+    '6:50'
+];
+
+
+itemsHours.map((el, index) => {
+    const time = el.textContent.trim()
+    const img = el.children[0]
+
+    // Acá el restar las horas 
+    // setInterval(() => {
+    //     let timeCurrent = moment().format('h:mma')
+    //     // console.log(timeCurrent)
+       
+    //     let beginningTime = moment(stopHours[index]+'am', 'h:mma');
+    //     let endTime = moment(timeCurrent, 'h:mma');
+    //     let valid = beginningTime.isBefore(endTime)
+    //     if (valid) {
+    //         el.textContent = time
+    //     } else {
+    //         // el.textContent = time
+    //         el.textContent = '-- : --'
+    //     }
+    //     // console.log(beginningTime.isBefore(endTime)); // true
+    // }, 1000);
+
+
+    el.addEventListener('click', (e) => {
+        e.preventDefault()
+        if (img.style.opacity == '' || img.style.opacity == '0') {
+            img.style.opacity = '1'
+            hoursSelected.push( time )
+        } else {
+            img.style.opacity = '0'
+            hoursSelected = hoursSelected.filter(x => {
+                if (x != time) { return x }
+            })
+        }
+        hoursSelectedFunc()
+    })
+})
+function hoursSelectedFunc() {
+    hoursSelectedForm.innerHTML = ''
+    hoursSelected.map(el => {
+        const itemHourSelected = document.createElement('div')
+        itemHourSelected.classList.add('item-hour-selected')
+        itemHourSelected.textContent = el
+        hoursSelectedForm.appendChild(itemHourSelected)
+    })
+}
+const itemsAnimals = [...document.querySelectorAll('.grid-animals .item-animals')]
+itemsAnimals.map((el, index) => {
+    el.addEventListener('click', (e) => {
+        if (hoursSelected.length > 0) {
+            e.preventDefault()
+            const pathAnimalCurrent = el.querySelector('img').getAttribute('src')
+            const name = el.querySelector('img').getAttribute('alt')
+            closeModalAnimals.parentElement.parentElement.style.display = 'flex'
+            currentAnimal.setAttribute('src', pathAnimalCurrent)
+            currentAnimal.setAttribute('alt', name)
+        } else {
+            alert('Debe seleccionar al menos una hora primero')
+        }
+    })
+})
+// Today Date
+const date = new Date();
+let day = date.getDate();
+let month = date.getMonth() + 1;
+let year = date.getFullYear();
+let currentDate = `${day}-${month}-${year}`;
+const dateCurrentDom = document.querySelector('.form-total .date span')
+dateCurrentDom.innerHTML = currentDate
+
+// Final Form Total
+const btnSendFormTotal = document.querySelector('.form-total .btn-send')
+btnSendFormTotal.addEventListener('click', (e) => {
+    e.preventDefault();
+    const itemsAnimalsSelectedFinal = [...document.querySelectorAll('.item-animals-selected')]
+    const itemsHoursSelectedFinal = [...document.querySelectorAll('.item-hour-selected')]
+    let valueTotal = []
+    let animalsFinal = []
+    let hoursSelectedFinal = []
+    itemsAnimalsSelectedFinal.map(el => {
+        valueTotal.push(parseInt(el.querySelector('span').textContent.trim()))
+        animalsFinal.push(el.querySelector('div').textContent.trim())
+    })
+    itemsHoursSelectedFinal.map(el => {
+        hoursSelectedFinal.push(el.textContent)
+    })
+    const total = valueTotal.reduce((previous, current) => {
+        return parseInt(previous) + parseInt(current);
+    }, 0);
+    if (total <= myBalance) {
+        alert('Felicidades por su compra!!')
+        // Acá está un array donde puede ver todos los datos que ha enviado en usuario 'formFinal'
+        const formFinal = [hoursSelectedFinal, animalsFinal, currentDate]
+        console.log(formFinal)
+    } else {
+        alert('Debe recargar su saldo para hacer las apuestas, verifique su balance por favor...')
+    }
+})
+
