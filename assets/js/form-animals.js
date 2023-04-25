fe544ee8e24713ce52bfdc8e191d13eaf4765fd2
bellaciaodowhile <125389712+bellaciaodowhile@users.noreply.github.com>
2023-04-25 23:44:22 +0200

    Final modal total

--- assets/js/form-animals.js
+++ assets/js/form-animals.js
@@ -120,6 +120,7 @@ itemsHours.map((el, index) => {
         let first = new moment(timeVnzlaCurrent, 'h:mma')
         let two = new moment(timesPoints._i, 'h:mma')
         if (first > two) {
+            el.style.display = 'none'
             el.textContent = '-- : --'
             el.setAttribute('time', '-- : --')
             el.classList.add('inactive')
@@ -230,7 +231,10 @@ btnSendFormTotal.addEventListener('click', (e) => {
     let hoursSelectedFinal = []
     itemsAnimalsSelectedFinal.map(el => {
         valueTotal.push(parseInt(el.querySelector('span').textContent.trim()))
-        animalsFinal.push(el.querySelector('div').textContent.trim())
+        animalsFinal.push({
+            name: el.querySelector('div').textContent.trim(),
+            imgPath: el.querySelector('img').getAttribute('src') 
+        })
     })
     itemsHoursSelectedFinal.map(el => {
         hoursSelectedFinal.push(el.textContent)
@@ -242,15 +246,32 @@ btnSendFormTotal.addEventListener('click', (e) => {
         if (animalsFinal.length > 0) {
             if (total <= myBalance) {
                 const formFinal = [hoursSelectedFinal, animalsFinal, currentDate] // Acá está un array donde puede ver todos los datos que ha enviado en usuario 'formFinal'
-                let finalFormText = `+ Fecha: `
-                finalFormText += formFinal[2] + ' \n'
-                finalFormText += `+ Horarios: \n`
-                formFinal[0].map(hour => finalFormText += `- ${hour} \n`)
-                finalFormText += `+ Jugadas: \n`
-                formFinal[1].map(animal => finalFormText += `- ${animal} Procesado \n`)
-                finalFormText += `+ Total: ${total}\n`
-                finalFormText += '¡¡Gracias por su compra!!'
-                alert(finalFormText)
+                const itemsHoursFormFinal = document.querySelector('.hours-final-form')
+                const itemsAnimalsFormFinal = document.querySelector('.animals-final-form')
+                const fechaFinal = document.querySelector('.fecha-final span')
+                const finalTotal = document.querySelector('.final-total')
+                closeModalAnimals.parentElement.parentElement.style.display = 'flex'
+                formModal.style.display = 'none'
+                formFinalContent.style.display = 'block'
+                fechaFinal.innerHTML = ''
+                itemsHoursFormFinal.innerHTML = ''
+                itemsAnimalsFormFinal.innerHTML = ''
+                finalTotal.innerHTML = ''
+                fechaFinal.innerHTML = formFinal[2]
+                formFinal[0].map(x => {
+                    itemsHoursFormFinal.innerHTML += `
+                    <div class="item-hour-selected">
+                        ${ x }
+                    </div>`
+                })
+                formFinal[1].map(x => {
+                    itemsAnimalsFormFinal.innerHTML += `
+                    <div class="item-animals-selected">
+                        <img src="${ x.imgPath }">
+                        <div>${ x.name } <span><small>procesado</small></span></div>
+                    </div>`
+                })
+                finalTotal.innerHTML = ' Total: ' + total + ',00'
             } else {
                 alert('Debe recargar su saldo para hacer las apuestas, verifique su balance por favor...')
             }
@@ -260,4 +281,40 @@ btnSendFormTotal.addEventListener('click', (e) => {
     }  else {
         alert('Debe seleccionar una hora')
     }
+})
+// Final Form Validate
+const btnSendFinalForm = document.querySelector('.form-final-content .btn-send-final-form')
+btnSendFinalForm.addEventListener('click', (e) => {
+    e.preventDefault();
+    formFinalContent.innerHTML = `
+    <div class='checkmark-animals'>
+        <svg class='confetti' height='19' viewBox='0 0 19 19' width='19' xmlns='http://www.w3.org/2000/svg'>
+            <path d='M8.296.747c.532-.972 1.393-.973 1.925 0l2.665 4.872 4.876 2.66c.974.532.975 1.393 0 1.926l-4.875 2.666-2.664 4.876c-.53.972-1.39.973-1.924 0l-2.664-4.876L.76 10.206c-.972-.532-.973-1.393 0-1.925l4.872-2.66L8.296.746z' fill='#00C8E5'>
+        </svg>
+        <svg class='confetti' height='19' viewBox='0 0 19 19' width='19' xmlns='http://www.w3.org/2000/svg'>
+            <path d='M8.296.747c.532-.972 1.393-.973 1.925 0l2.665 4.872 4.876 2.66c.974.532.975 1.393 0 1.926l-4.875 2.666-2.664 4.876c-.53.972-1.39.973-1.924 0l-2.664-4.876L.76 10.206c-.972-.532-.973-1.393 0-1.925l4.872-2.66L8.296.746z' fill='#00C8E5'>
+        </svg>
+        <svg class='confetti' height='19' viewBox='0 0 19 19' width='19' xmlns='http://www.w3.org/2000/svg'>
+            <path d='M8.296.747c.532-.972 1.393-.973 1.925 0l2.665 4.872 4.876 2.66c.974.532.975 1.393 0 1.926l-4.875 2.666-2.664 4.876c-.53.972-1.39.973-1.924 0l-2.664-4.876L.76 10.206c-.972-.532-.973-1.393 0-1.925l4.872-2.66L8.296.746z' fill='#00C8E5'>
+        </svg>
+        <svg class='confetti' height='19' viewBox='0 0 19 19' width='19' xmlns='http://www.w3.org/2000/svg'>
+            <path d='M8.296.747c.532-.972 1.393-.973 1.925 0l2.665 4.872 4.876 2.66c.974.532.975 1.393 0 1.926l-4.875 2.666-2.664 4.876c-.53.972-1.39.973-1.924 0l-2.664-4.876L.76 10.206c-.972-.532-.973-1.393 0-1.925l4.872-2.66L8.296.746z' fill='#00C8E5'>
+        </svg>
+        <svg class='confetti' height='19' viewBox='0 0 19 19' width='19' xmlns='http://www.w3.org/2000/svg'>
+            <path d='M8.296.747c.532-.972 1.393-.973 1.925 0l2.665 4.872 4.876 2.66c.974.532.975 1.393 0 1.926l-4.875 2.666-2.664 4.876c-.53.972-1.39.973-1.924 0l-2.664-4.876L.76 10.206c-.972-.532-.973-1.393 0-1.925l4.872-2.66L8.296.746z' fill='#00C8E5'>
+        </svg>
+        <svg class='confetti' height='19' viewBox='0 0 19 19' width='19' xmlns='http://www.w3.org/2000/svg'>
+            <path d='M8.296.747c.532-.972 1.393-.973 1.925 0l2.665 4.872 4.876 2.66c.974.532.975 1.393 0 1.926l-4.875 2.666-2.664 4.876c-.53.972-1.39.973-1.924 0l-2.664-4.876L.76 10.206c-.972-.532-.973-1.393 0-1.925l4.872-2.66L8.296.746z' fill='#00C8E5'>
+        </svg>
+        <svg class='checkmark__check' height='36' viewBox='0 0 48 36' width='48' xmlns='http://www.w3.org/2000/svg'>
+            <path d='M47.248 3.9L43.906.667a2.428 2.428 0 0 0-3.344 0l-23.63 23.09-9.554-9.338a2.432 2.432 0 0 0-3.345 0L.692 17.654a2.236 2.236 0 0 0 .002 3.233l14.567 14.175c.926.894 2.42.894 3.342.01L47.248 7.128c.922-.89.922-2.34 0-3.23'>
+        </svg>
+        <svg class='checkmark__back' height='115' viewBox='0 0 120 115' width='120' xmlns='http://www.w3.org/2000/svg'>
+            <path d='M107.332 72.938c-1.798 5.557 4.564 15.334 1.21 19.96-3.387 4.674-14.646 1.605-19.298 5.003-4.61 3.368-5.163 15.074-10.695 16.878-5.344 1.743-12.628-7.35-18.545-7.35-5.922 0-13.206 9.088-18.543 7.345-5.538-1.804-6.09-13.515-10.696-16.877-4.657-3.398-15.91-.334-19.297-5.002-3.356-4.627 3.006-14.404 1.208-19.962C10.93 67.576 0 63.442 0 57.5c0-5.943 10.93-10.076 12.668-15.438 1.798-5.557-4.564-15.334-1.21-19.96 3.387-4.674 14.646-1.605 19.298-5.003C35.366 13.73 35.92 2.025 41.45.22c5.344-1.743 12.628 7.35 18.545 7.35 5.922 0 13.206-9.088 18.543-7.345 5.538 1.804 6.09 13.515 10.696 16.877 4.657 3.398 15.91.334 19.297 5.002 3.356 4.627-3.006 14.404-1.208 19.962C109.07 47.424 120 51.562 120 57.5c0 5.943-10.93 10.076-12.668 15.438z' fill='#00C8E5'>
+        </svg>
+    </div>
+    <h2 class="text-center text-white mt-5">¡Gracias por su compra!</h2>`
+    setTimeout(() => {
+        window.location.reload();
+    }, 2000);
 })
